6e7536af5e28bc627dcb2b16b77431a7e620db94
sapWRLD <tomkerstens27@gmail.com>
2025-10-07 19:54:52 +0200

    copyright changes

--- js/copyright.js
+++ js/copyright.js
@@ -2,5 +2,5 @@ const text = document.getElementById("copyright");
 const date = new Date();
 
 addEventListener("DOMContentLoaded", () => {
-    text.textContent = `©${date.getFullYear()} Arctiq. All right reserved.`; 
+    text.textContent = `©${date.getFullYear()} Arctiq. All rights reserved.`; 
 })
